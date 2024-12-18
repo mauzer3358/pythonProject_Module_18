972b200b974b428462fa32d9a4d9821a0c43fb83
Mauzer33 <mauzer3358@gmail.com>
2024-12-18 11:24:02 +0300

    Initial commit

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -6,7 +6,17 @@ def main_page(request):
     return render(request, 'main_template.html')
 
 def shop_page(request):
-    return render(request, 'shop_template.html')
+    title = 'Shop'
+    item1 = "Atomic Heart"
+    item2 = "Cyberpunk 2077"
+    item3 = "PayDay 2"
+    context = {
+        'title': title,
+        'item1': item1,
+        'item2': item2,
+        'item3': item3
+    }
+    return render(request, 'shop_template.html', context)
 
 def cart_page(request):
     return render(request, 'cart_template.html')
